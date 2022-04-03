096996d15e8311614a6197f8a821052172b4638a
Li-Haowei <haoweilee666@gmail.com>
2022-04-03 00:41:27 -0400

    creating data with http post

--- src/app/posts.component.ts
+++ src/app/posts.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-posts',
     template: `
+    <input 
+    (keyup.enter) = "createPost(input)" #input
+    type="text" class = "form-control">
+
     <ul class="list-group">
         <li *ngFor="let post of posts"
         class="list">
@@ -14,11 +18,27 @@ import { Component } from '@angular/core'
 })
 export class PostsComponent {
     posts: any[];
-    constructor(http: HttpClient){
+    private url = 'https://jsonplaceholder.typicode.com/posts';
+    constructor(private http: HttpClient){
         this.posts = [];
-        http.get('https://jsonplaceholder.typicode.com/posts')
+        //http get: Get data
+        http.get(this.url)
         .subscribe(response =>{
             this.posts = response as any[];
         });
     }
+    createPost(input: HTMLInputElement){
+        let post = {title: input.value}
+        input.value = '';
+        //http post: Create data
+        //first input server, second input json file
+        this.http.post(this.url, JSON.stringify(post))
+        .subscribe(response =>{
+            post['id'] = response['id'];
+            this.posts.splice(0,0,post);
+            
+        })
+    }
+    //http put: update data
+    //http delete: delete data
 }
