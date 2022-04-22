4f128d741569bbf8c1fc3daedc354540be14b795
Li-Haowei <haoweilee666@gmail.com>
2022-04-22 02:17:53 -0400

    working on 3D models now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //This allows two way binding [()] = "example"
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { PostsComponent } from './posts.component';
-import { CoursesService } from './courses.service';
-import { SummaryPipe } from 'src/summary.pipe';
 import {HttpClientModule} from '@angular/common/http';
-import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    PostsComponent,
-    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule, //This allows two way binding [()] = "example"
     HttpClientModule
   
   ],
   providers: [
-    PostService,
-    CoursesService,
     HttpClientModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
