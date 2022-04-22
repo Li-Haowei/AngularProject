c6c561920a1a151168a6e99c443a40d8ce9bb572
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2022-04-22 02:36:33 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  name = `Angular`;
+  
+  camera_orbit = '0deg 90deg 100m';
+  src = "assets/teddy.glb";//'https://modelviewer.dev/shared-assets/models/Astronaut.glb';//
 }
